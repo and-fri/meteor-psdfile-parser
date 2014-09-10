bfaead41b30e38634728e1d3c3a7c7b082bafa48
andrej fritz <a.fritz@betzold.de>
2014-09-10 16:48:06 +0200

    updated to meteor 0.9

--- package.js
+++ package.js
@@ -1,11 +1,13 @@
 Package.describe({
   name: "psdfile-parser",
   summary: "A Photoshop file format (PSD) parser. Can export PNGs and Thumbnails..."
+  git: "https://github.com/andruschka/meteor-psdfile-parser.git"
 });
 Npm.depends({
   "png":"3.0.3",
   "gm":"1.16.0"
 });
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.add_files('psd.js', ['server','client']);
 });
